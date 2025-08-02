6c5fd1c9f53c4e4e20aaaf4e79b04b8ad2269b88
Michał Moniewski <mmoniewski@protonmail.com>
2025-08-02 23:40:08 +0200

    Add pause to pause menu

--- src/ui/pause_menu.gd
+++ src/ui/pause_menu.gd
@@ -3,10 +3,12 @@ extends CanvasLayer
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		visible = not visible
+		get_tree().paused = not get_tree().paused
 
 
 func _on_continue_pressed() -> void:
 	visible = false
+	get_tree().paused = false
 
 func _on_quit_to_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
@@ -17,3 +19,7 @@ func _on_quit_to_desktop_pressed() -> void:
 
 func _on_toggle_full_screen_pressed() -> void:
 	GLOB.settings.fullscreen = not GLOB.settings.fullscreen
+
+# Unpause on quit to menu and other unexpected events
+func _exit_tree() -> void:
+	get_tree().paused = false
