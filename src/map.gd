da9340c26513a23fac6da9a30a8aa3a7ffc1b60c
borb <borbware@gmail.com>
2025-08-03 10:11:19 +0200

    add better spawn positions, smaller radius for corpses

--- src/map.gd
+++ src/map.gd
@@ -4,25 +4,22 @@ extends Node2D
 @onready var mushroom_scene = preload("res://scenes/mushroom.tscn")
 @onready var cursor_scene = preload("res://scenes/cursor.tscn")
 
-# TODO VERY TEMP
-@export var mushroom_positions: Array[Vector2] = [
-	Vector2(200, 300),
-	Vector2(400, 500),
-	Vector2(100, 600),
-	Vector2(700, 400),
-]
-
 func _ready():
+	var spawn_locations = get_node("SpawnLocations")
+
 	for player_id in range(GLOB.settings.player_settings.size()):
 		GLOB.players_in_the_game += 1
 		var mushroom: Mushroom = mushroom_scene.instantiate()
 		mushroom.player_id = player_id
-		mushroom.position = mushroom_positions[player_id]
+
+		var pos = spawn_locations.get_child(player_id - 1).position
+
+		mushroom.position = pos
 		add_child(mushroom)
 		
 		var cursor: Cursor = cursor_scene.instantiate()
 		cursor.player_id = player_id
-		cursor.position = mushroom_positions[player_id]
+		cursor.position = pos
 		cursor.color = GLOB.player_colors[player_id]
 		add_child(cursor)
 
